a1f394fcc50af7506a6486d13aa3215411c33419
Nirajan Mahara <mnirajan9@gmail.com>
2021-10-26 09:33:05 +0545

    `const app = express()` & listen to `PORT` with `callback function`

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const PORT = process.env.PORT || 8000;
 const cheerio = require('cheerio');
 const express = require('express');
 const axios = require('axios');
+
+const app = express();
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
